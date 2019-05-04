53c87afdae818403773adf2d0f9f018a5f812838
katherinestout <kjs14@hood.edu>
2019-05-03 20:18:01 -0400

    add

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,7 +4,11 @@ import React, { Component } from 'react';
 
 class App extends Component {
   state = {
-    flavors: []
+    flavors: [],
+    flavor: {
+      type: '',
+      price: ''
+    }
   }
   componentDidMount(){
     this.getFlavors();
@@ -13,19 +17,36 @@ class App extends Component {
   getFlavors =()=> {
    fetch("http://localhost:5000/flavors")
    .then(response => response.json())
-   .then(({data}) => {
-     console.log(data)}).catch(err => console.log(err))
+   .then(response => this.setState({flavors: response.data}))
+   .catch(err => console.log(err))
+  }
+
+  addFlavor = () => {
+    const { flavor } = this.state;
+    fetch(`http://locahost:5000/flavors/add?type='${flavor.type}'&price=${flavor.price}`)
+    .then(response => response.json())
+    .then(this.getFlavors)
+    .catch(err => console.log(err))
   }
 
   renderFlavor = ({type_id, type}) => <div key={type_id}>{type}</div>
+
   
   render() {
 
-    const {flavors} = this.state;
+    const {flavors, flavor} = this.state;
 
     return (
-      <div>
+      <div className="App">
         {flavors.map(this.renderFlavor)}
+        <input value={flavor.type}
+        onChange={e => this.setState({flavor: {...flavors, type: e.target.value}})}
+        />
+        <input value={flavor.price}
+         onChange={e => this.setState({flavor: {...flavors, price: e.target.value}})}
+        />
+
+        <button onClick={this.addFlavor} > Add a flavor!</button>
       </div>
     )
   }
