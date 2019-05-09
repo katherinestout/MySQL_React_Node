bb59ae6cd903ca1d59b3b6498b63a7f63eb77aaf
katherinestout <kjs14@hood.edu>
2019-05-09 13:12:26 -0400

    my-app not client

--- server.js
+++ server.js
@@ -20,9 +20,9 @@ app.use(routes);
 //serve static assets in productions
 if(process.env.NODE_ENV === 'production'){
     //set static folder
-    app.use(express.static('client/build'));
+    app.use(express.static('my-app/build'));
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, 'my-app', 'build', 'index.html'));
     })
 }
 
